ab1eefa1e6338fe2710a056d481c6fabec1e6453
alen66 <centr-opt-pavl@yandex.ru>
2020-02-28 15:07:54 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,66 +4,54 @@ var close = popup.querySelector(".close");
 var user = popup.querySelector("[name=user]");
 var mail = popup.querySelector("[name=mail]");
 var form = popup.querySelector("form");
-  var isStorageSupport = true;
-  var storage = "";
+var isStorageSupport = true;
+var storage = "";
 
 
-
-
-
-
-
-
-  try {
-    storage = localStorage.getItem("user");
-  } catch (err) {
-    isStorageSupport = false;
-  }
+try {
+  storage = localStorage.getItem("user");
+} catch (err) {
+  isStorageSupport = false;
+}
 
 link.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.add("modal-show");
-    
-    if (storage) {
-      user.value = storage;
-      mail.focus();
-    } else {
-      user.focus();
-    }
-  });
+  evt.preventDefault();
+  popup.classList.add("modal-show");
 
-  close.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.remove("modal-show");
+  if (storage) {
+    user.value = storage;
+    mail.focus();
+  } else {
+    user.focus();
   }
-  )
-  ;
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+
+});
 
 
- form.addEventListener("submit", function (evt) {
-    if (!user.value || !mail.value) {
-      evt.preventDefault();
-        } else {
-      if (isStorageSupport) {
-        localStorage.setItem("user", user.value);
-      }
-    }
-  });
-  
-    window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      evt.preventDefault();
-      if (popup.classList.contains("modal-show")) {
-        popup.classList.remove("modal-show");
-      }
-    }
-  });
+form.addEventListener("submit", function (evt) {
+  if (!user.value || !mail.value) {
+    evt.preventDefault();
+    
 
-    window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      evt.preventDefault();
-      if (pop.classList.contains("activ-show")) {
-        pop.classList.remove("activ-show");
-      }
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("user", user.value);
     }
-  });
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+     
+    }
+  }
+});
+
